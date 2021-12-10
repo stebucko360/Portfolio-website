1bf2c398744b6a021cbfe08e1c8750124b1751f5
stebucko360 <stebucko360@gmail.com>
2021-12-10 23:27:36 +0000

    heading bar created

---
+++ portfolio-site/src/components/Heading.jsx
@@ -0,0 +1,21 @@
+import githublogo from "../images/whitegit.png"
+import instagramlogo from "../images/instagramwhite.png"
+
+export const Heading = () => {
+    return (
+        <div className='heading'>
+            <div className='namecontainer'>
+            <h1 className='mainbartitle'> Stephen Buckley</h1>
+            <h2 className='mainportfolio'>Portfolio</h2>
+            </div>
+            <div className='logo'><span className='constlogo'>const</span>
+            <span className='equals'>=</span>
+            <span className='arrow'>&lt;</span> SB Development <span className='arrow'>/&gt;</span> 
+            </div>
+            <div className='medialinks'>
+                <img className='githublogo' src={githublogo} alt='github'></img>
+                <img className='instalogo' src={instagramlogo} alt='instagram'></img>
+            </div>
+        </div>
+    )
+}
