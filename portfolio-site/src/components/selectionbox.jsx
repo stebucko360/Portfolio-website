a865082c28dc7987e3215379dc02c135ff878b26
stebucko360 <stebucko360@gmail.com>
2021-12-13 18:44:10 +0000

    reformatted boxes and heading bar thinned

--- portfolio-site/src/components/selectionbox.jsx
+++ portfolio-site/src/components/selectionbox.jsx
@@ -4,10 +4,10 @@ export const Selectionbox = () => {
             <div1></div1>
             <section/>
             <div2 className='selectionbox'>
-                <button>Portfolio</button>
-                <button>About</button>
-                <button>Contact</button>
-                <button>Skills/experience</button>
+                <button id='portfoliobutton'>Portfolio</button>
+                <button id='aboutbutton'>About</button>
+                <button id='contactbutton'>Contact</button>
+                <button id='skillsbutton'>Skills/experience</button>
             </div2>
             <div3></div3>
         </div>
